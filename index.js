6f13c7f27b4644270a3457d0e4e396dee01032b4
Hamsikakrishnan <r.hamsikakrishnan@gmail.com>
2025-07-01 14:01:09 +0530

    Changes Done

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
 const modalText = document.getElementById('modal-text')
-const modalInnerText = document.getElementById('modal-inner')
+const declineBtn = document.getElementById('decline-btn')
+const modalChoiceBtns = document.getElementById('modal-choice-btns')
 
 setTimeout(function(){
     modal.style.display = 'inline'
@@ -12,8 +13,16 @@ modalCloseBtn.addEventListener('click', function(){
     modal.style.display = 'none'
 })
 
+declineBtn.addEventListener('mouseenter', function(){
+    modalChoiceBtns.classList.toggle('modal-choice-btns-reverse')
+})
+
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
+    
+    const consentFormData = new FormData(consentForm)
+    const fullName = consentFormData.get('fullName')
+    
     modalText.innerHTML = `
     <div class="modal-inner-loading">
         <img src="images/loading.svg" class="loading">
@@ -21,17 +30,20 @@ consentForm.addEventListener('submit', function(e){
     </div>` 
     
     setTimeout(function(){
-        document.getElementById('upload-text').innerText = "Making the sale..."
-        setTimeout(function(){
-            modalInnerText.innerHTML = `<h2>Thanks you sucker! </h2>
+        document.getElementById('upload-text').innerText = `
+        Making the sale...`
+    }, 1500)
+    
+    
+    setTimeout(function(){
+        document.getElementById('modal-inner').innerHTML = `
+        <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
         <p>We just sold the rights to your eternal soul.</p>
         <div class="idiot-gif">
             <img src="images/pirate.gif">
         </div>
-        ` 
-        }, 1500)
-    }, 1500)
-    
-    
-
+    `
+    modalCloseBtn.disabled = false
+    }, 3000)
+  
 }) 
